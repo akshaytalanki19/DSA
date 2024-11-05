eb4d7ac505fcfe1c2f169a831892588a06e9cf8d
Akshay <akshaytalanki19@gmail.com>
2024-11-05 22:29:13 +0530

    day-2 of doing dsa

---
+++ basics/expression.java
@@ -0,0 +1,16 @@
+public class expression {
+    public static void main(String[] args) {
+        byte b=4;
+        char c='a';
+        short s=512;
+        int i=1000;
+        float f= 3.14f;
+        double d= 99.9954;
+        System.out.println((f*b)+(i%c)-(d*s));
+        System.out.println((f*b));
+        System.out.println((i%c));
+        System.out.println((d*s));
+        int $=23;
+        System.out.println($);
+    }
+}
---
+++ basics/typePrommotion.java
@@ -0,0 +1,8 @@
+public class typePrommotion {
+    public static void main(String[] args) {
+        double d=12.3456;
+        long l= 123456;
+        float f= 12.4321f;
+        System.out.println(d+l+f); // promotes the values to double
+    }
+}
