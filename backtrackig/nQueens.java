5a7d93d6fea3bc64c2aa841502d006ca7679d0e7
AKSHAY KUMAR <akshaytalanki19@gmail.com>
2025-03-23 23:16:29 +0530

    nqueens problem

---
+++ backtrackig/nQueens.java
@@ -0,0 +1,62 @@
+public class nQueens {
+    static int count=0;
+    public static void main(String[] args) {
+        int n=16;
+        char board[][] = new char[n][n];
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                board[i][j]='.';
+            }
+        }
+        printBoard(board);
+        nQueen(board,0);
+        System.out.println(count-1);
+    }
+    public static void nQueen(char board[][], int row){
+         if(row==board.length){
+            printBoard(board);
+            return;
+         }
+        for(int j=0;j<board.length; j++){
+            if(isSafe(board,row,j)){
+            board[row][j]= 'Q';
+            nQueen(board,row+1);
+            board[row][j]='.';
+            }
+         }
+    }
+    public static boolean isSafe(char board[][], int row,int col){
+        //vertical up
+        for(int i=row-1;i>=0;i--){
+            if(board[i][col] == 'Q'){
+                return false;
+            }
+        }
+        //diag left
+        for(int i=row-1,j=col-1;i>=0 && j>=0;i--,j--){
+            if(board[i][j] == 'Q'){
+                return false;
+            }
+        }
+        //diag right
+       
+        for(int i=row-1, j=col+1;i>=0 && j<board.length;i--,j++){
+            if(board[i][j]=='Q'){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void printBoard(char board[][]){
+        count++;
+        System.out.println("----chesss board----");
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board.length;j++){
+                System.out.print(board[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+
+}
